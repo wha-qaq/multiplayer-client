e88648659be4a1253a21259c913d6ccb9d7b877c
joaquinta <163373067+joaquinta@users.noreply.github.com>
2025-05-07 12:15:11 +1000

    Player Auth Part 1

--- Source/player_auth.gd
+++ Source/player_auth.gd
@@ -1,7 +1,19 @@
-extends Node
+extends HTTPRequest
+
+const DOMAIN = "localhost:5000"
+const LOGIN = DOMAIN + "/login"
+
+var auth : String = ""
 
 func can_login() -> bool:
 	return true
 
+func save_token():
+	pass
+
+func get_token():
+	pass
+	#auth = $HTTPRequest.request("localhost:5000").send_req
+
 func _ready() -> void:
-	pass # Replace with function body.
+	print(": ", request("https://example.com"))
