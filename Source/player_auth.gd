40646b0723a8b97c812f155de482002fd51a7a4b
aaa <163373067+wha-qaq@users.noreply.github.com>
2025-05-15 13:20:49 +1000

    Client Authentication part 1
    Client now properly handles logging in

--- Source/player_auth.gd
+++ Source/player_auth.gd
@@ -1,35 +1,47 @@
 extends Node
 
-const DOMAIN = "https://localhost:5000"
-const LOGIN = DOMAIN + "/login?username=%s&password=%s"
+const DOMAIN = "http://127.0.0.1:5000"
+const LOGIN = DOMAIN + "/login?username=%s&password=%s" # TODO: parse it securely
 
-var request : HTTPRequest
+var request : HTTPRequest = HTTPRequest.new()
 var auth : String = ""
 
 func can_login() -> bool:
+	if not auth:
+		return false
 	return true
 
-func save_token():
-	pass
+func save_token(token : String):
+	auth = token
+	# TODO: Saving token
 
-func request_token(username, password):
-	#var error = request.request(LOGIN % [username, password], [], HTTPClient.METHOD_POST)
+func request_login(username, password):
 	request.request_completed.connect(_token_recieved)
-	var error = request.request("http://example.com")
+	var error = request.request(LOGIN % [username, password], [], HTTPClient.METHOD_POST)
+	#var error = request.request("https://example.com/")
 	return error
 
-func _token_recieved(error, status, headers, d):
-	if error != OK:
+func _token_recieved(result, response_code, headers, body : PackedByteArray):
+	if result != OK:
 		return
-	if error != status:
+	var json = JSON.new()
+	var err = json.parse(body.get_string_from_utf8())
+	if err:
+		print("Invalid response")
 		return
-	if not ("Content-Type: text/html" in headers):
+	var response = json.get_data()
+	if not (response is Dictionary):
+		print("Invalid response")
 		return
-	print(d)
+	if response_code != HTTPClient.RESPONSE_OK:
+		print("Error given: ", response.get("error"))
+		return
+	if not response.get("token"):
+		print("Token not found")
+		return
+	save_token(response["token"])
 
 func _ready() -> void:
-	request = HTTPRequest.new()
 	add_child(request)
 	
-	print(request_token("hello", "world"))
-	print(auth)
+	request_login("hello", "world") # NOTE: Test code, delete later
